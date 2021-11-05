ff213d2658c9fdfbd3c3cfc8a0407a842c56f745
hanuman <a.kolyan@gratun.am>
2021-11-05 14:27:59 +0400

    end of 1 lesson

--- Lesson 1/2 intro to p5/sketch.js
+++ Lesson 1/2 intro to p5/sketch.js
@@ -1,3 +1,12 @@
 function setup() {
+    createCanvas(1000,200)
+    background("blak")
+    noStroke()
+    rect(100,200,50,60)
 
+    stroke("red")
+
+    fill("blue")
+
+    ellipse(10, 20, 100, 200)
 }
