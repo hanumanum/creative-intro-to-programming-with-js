5f12d7fc44b3b9bcd7dce7c94976fe3c7ab5b439
hanuman <a.kolyan@gratun.am>
2021-12-04 22:03:33 +0400

    lesson 10 start package

---
+++ Lesson 10/1 Objects and Classes/sketch.js
@@ -0,0 +1,46 @@
+let w = 800
+let h = 800
+
+//Change this to CLASS object 
+let circle1 = {
+    x:400,
+    y:400,
+    r:150,
+    clr:"violet"
+}
+
+let circle2 = {
+    x:400,
+    y:400,
+    r:80,
+    clr:"red"
+}
+
+let circle3 = {
+    x:200,
+    y:560,
+    r:80,
+    clr:"white"
+}
+
+function setup() {
+    createCanvas(w, h)
+    background("black")
+}
+
+function draw() {
+    background("black")
+
+    fill(circle1.clr)
+    ellipse(circle1.x,circle1. y, circle1.r)
+    
+    fill(circle2.clr)
+    ellipse(circle2.x,circle2. y, circle2.r)
+
+    fill(circle1.clr)
+    ellipse(circle3.x,circle3. y, circle3.r)
+
+    circle1.x++
+    circle2.x++
+    circle3.x++
+}
---
+++ Lesson 10/2 CASE STUDY Game/sketch.js
@@ -0,0 +1,11 @@
+let w = 1000
+let h = 800
+
+function setup() {
+    createCanvas(w, h)
+    background("black")
+}
+
+function draw() {
+    background("black")
+}
